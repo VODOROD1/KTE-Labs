048de5a5cf9c6ee4ff436fbf6b55ea2f9d97497e
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-03-28 11:23:14 +0400

    refactoring

--- src/app/components/full-item/full-item.component.ts
+++ src/app/components/full-item/full-item.component.ts
@@ -7,35 +7,13 @@ import { StoreService } from 'src/app/services/store-service.service';
   templateUrl: './full-item.component.html',
   styleUrls: ['./full-item.component.scss'],
 })
+
 export class FullItemComponent implements OnInit {
+  public item: ItemClass;
+
   constructor(private storeService: StoreService) {}
 
-  ngOnInit() {}
-
-  formSubmitHandler(data: any) {
-    console.log(data);
-    debugger;
-
-    let newObj: ItemClass = {
-      id: 4,
-      title: data.title,
-      amount: data.amount,
-      description: data.description,
-      shortDescription: data.shortDescription,
-      attributes: [
-        `${data.title}1`,
-        `${data.title}2`,
-        `${data.title}3`,
-        `${data.title}4`,
-        `${data.title}5`,
-      ],
-      price: data.price,
-      createdAt: data.date,
-      image:
-        'http://archive.brightmedia.ua/userfiles/image/1td/2009_prigotov/pack-prigotov.jpg',
-    };
-
-    this.storeService.addItem(newObj);
-    debugger;
+  ngOnInit() {
+    this.item = this.storeService.getItem(1);
   }
 }
